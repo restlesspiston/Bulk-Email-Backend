27965c19020057bbfcbe569cd343f6c2abb8087c
restlesspiston <smartworld69gurugram@gmail.com>
2024-08-20 22:43:35 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+require('dotenv').config();
+const express = require('express');
+const apiRoutes = require('./routes/api');
+const mongoose = require('mongoose');
+const os = require('os');
+
+const app = express();
+
+const cors = require('cors');
+
+app.use(cors({
+    origin: true,
+    credentials: true
+}));
+
+app.use(express.json());
+
+app.use(apiRoutes);
+
+// Function to get the server's IP address
+const getServerIpAddress = () => {
+    const networkInterfaces = os.networkInterfaces();
+    for (const interfaceName in networkInterfaces) {
+        for (const iface of networkInterfaces[interfaceName]) {
+            // Check for IPv4 and not loopback address
+            if (iface.family === 'IPv4' && !iface.internal) {
+                return iface.address;
+            }
+        }
+    }
+    return 'IP address not found';
+};
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('MongoDB connected');
+        const PORT = process.env.PORT || 8000;
+        app.listen(PORT, "0.0.0.0", () => {
+            const serverIp = getServerIpAddress();
+            console.log(`Server is running on http://${serverIp}:${PORT}`);
+        });
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+connectDB();
